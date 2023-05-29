7c24ab8e244ef64f7db94c19ad1b956188e46356
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-29 23:49:01 +0200

    create-react-app

---
+++ dictonary-project/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import image1 from "./image1.JPG";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={image1} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
