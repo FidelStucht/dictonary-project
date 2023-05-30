4a626cdbd549f68792f4292ebd15fd247eebc34b
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-30 21:42:49 +0200

    include API

--- dictonary-project/src/Dictonary.js
+++ dictonary-project/src/Dictonary.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 import "./Dictonary.css";
+import axios from "axios";
 
 export default function Dictonary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
-    alert(`Searching ${keyword}...`);
+    let key = "370f6ctbab902413o43b492fe5060b44";
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   return (
     <div className="Dictonary">
       <form onSubmit={search}>
